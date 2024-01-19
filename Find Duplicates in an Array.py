01fe4fe7275d8c16513cbe685b00cfd46d5d04ea
Bittu2903 <singhbittu2903@gmail.com>
2024-01-19 22:45:12 +0530

    gfg find duplicates in an array

---
+++ Find Duplicates in an Array.py
@@ -0,0 +1,58 @@
+# Given an array a of size N which contains elements from 0 to N-1, you need to find all the elements occurring more than once in the given array. Return the answer in ascending order. If no such element is found, return list containing [-1]. 
+
+# Note: The extra space is only for the array to be returned. Try and perform all operations within the provided array. 
+
+# Example 1:
+
+# Input:
+# N = 4
+# a[] = {0,3,1,2}
+# Output: 
+# -1
+# Explanation: 
+# There is no repeating element in the array. Therefore output is -1.
+# Example 2:
+
+# Input:
+# N = 5
+# a[] = {2,3,1,2,3}
+# Output: 
+# 2 3 
+# Explanation: 
+# 2 and 3 occur more than once in the given array.
+# Your Task:
+# Complete the function duplicates() which takes array a[] and n as input as parameters and returns a list of elements that occur more than once in the given array in a sorted manner. 
+
+# Expected Time Complexity: O(n).
+# Expected Auxiliary Space: O(n).
+
+# Constraints:
+# 1 <= N <= 105
+# 0 <= A[i] <= N-1, for each valid i
+
+class Solution:
+    def duplicates(self, arr, n): 
+        # Initialize a dictionary to store the frequency of each element
+        mpp = {}
+        
+        # Iterate through the array to populate the dictionary
+        for i in arr:
+            if i in mpp:
+                mpp[i] += 1
+            else:
+                mpp[i] = 1
+        
+        # Initialize an empty list to store duplicate elements
+        output = []
+        
+        # Iterate through the dictionary to find elements with frequency greater than 1
+        for i in mpp:
+            if mpp[i] > 1:
+                output.append(i)
+        
+        # If no duplicates are found, append -1 to the list
+        if len(output) == 0:
+            output.append(-1)
+        
+        # Return the sorted list of duplicates
+        return sorted(output)
